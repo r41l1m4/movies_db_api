b58448b1954275637bb428ba1b252e936dc91787
r41l1m4 <rai.lima17@gmail.com>
2023-04-27 13:26:54 -0300

    Initial commit

---
+++ src/test/java/dev/ironia/movies_db_api/MoviesDbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ironia.movies_db_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesDbApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
